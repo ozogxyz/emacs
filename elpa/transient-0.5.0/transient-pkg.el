87ce786f8286c5b028c55793a1d0275c9f080f94
Orkun Özoğlu <50372354+ozogxyz@users.noreply.github.com>
2023-11-29 20:32:51 +0300

    Clean up gitignore

--- elpa/transient-0.5.0/transient-pkg.el
+++
@@ -1,2 +0,0 @@
-;; Generated package description from transient.el  -*- no-byte-compile: t -*-
-(define-package "transient" "0.5.0" "Transient commands" '((emacs "26.1") (compat "29.1.4.4") (seq "2.24")) :commit "9050a0d058041e5e54d4e16359fd061e779c2ecb" :authors '(("Jonas Bernoulli" . "jonas@bernoul.li")) :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :keywords '("extensions") :url "https://github.com/magit/transient")
