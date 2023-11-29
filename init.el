3edbd3ad8c0e72013c6aa30221bdb5a3a02f6a5b
Orkun Özoğlu <50372354+ozogxyz@users.noreply.github.com>
2023-11-29 18:16:30 +0300

    Add elpa to gitignore

--- init.el
+++ init.el
@@ -1,20 +1,23 @@
 ;; Garbage collection
-(setq gc-cons-threshold 50000000)
-(setq large-file-warning-threshold 100000000)
+;; (setq gc-cons-threshold 50000000)
+;; (setq large-file-warning-threshold 100000000)
 
 ;; Move custom to its own file
-;; (setq custom-file "~/.emacs.d/custom.el")
-;; (load-file custom-file)
+(setq custom-file "~/.emacs.d/custom.el")
+(load-file custom-file)
 
 ;; Theme
 ;; (load-theme 'ef-cherie)
 ;; (load-theme 'modus-vivendi t)
 
+;; Font
+;; (set-face-attribute 'default t :font "Menlo Normal 14")
+
 ;; Starting directory
 ;; (setq default-directory "~/elixir")
 
 ;; GUI
-;; (setq inhibit-startup-message t)
+(setq inhibit-startup-message t)
 (setq pixel-scroll-precision-mode t)
 
 ;; Meta from Option to Cmd
@@ -22,8 +25,8 @@
 ;; (setq mac-command-modifier 'meta)
 
 ;; Text-mode
-;; (setq-default major-mode 'text-mode)
-;; (add-hook 'text-mode-hook 'auto-fill-mode)
+(setq-default major-mode 'text-mode)
+(add-hook 'text-mode-hook 'auto-fill-mode)
 
 ;; Package management
 (require 'package)
@@ -33,16 +36,10 @@
 ;; Vertico minibuffer
 (use-package vertico)
 
-;; ;; Erlang Mode
-;; (setq load-path (cons "/opt/local/lib/erlang/lib/tools-3.6/emacs" load-path))
-;; (setq erlang-root-dir "/opt/local/lib/erlang")
-;; (setq exec-path (cons "/opt/local/erlang/bin" exec-path))
-;; (require 'erlang-start)
-
 
 ;; Git
 (use-package magit
-  :bind (("C-M-g" . magit-status)))	
+  :bind (("C-M-g" . magit-status)))
 
 ;; Misc
 (fset 'yes-or-no-p 'y-or-n-p)
@@ -59,7 +56,7 @@
 
 
 ;; Company
-;; (add-hook 'after-init-hook 'global-company-mode)
+(add-hook 'after-init-hook 'global-company-mode)
 
 
 (custom-set-variables
@@ -67,10 +64,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("917d7e7f0483dc90a5e2791db980ce9bc39e109a29198c6e9bdcd3d88a200c33" "ee77d69f78a1a17dcd141a58367fb5534dfdb07e94924a10a03c54190bb6a0ef" default))
- '(package-selected-packages
-   '(helm yasnippet-snippets yasnippet company elixir-ts-mode magit vertico ef-themes)))
+ '(custom-safe-themes)
+ '(package-selected-packages))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
