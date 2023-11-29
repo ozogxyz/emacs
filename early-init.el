b294c912f9ba6e00738bf0713d91e9f1e29bc127
Orkun Özoğlu <50372354+ozogxyz@users.noreply.github.com>
2023-11-29 18:16:30 +0300

    add gitignore

--- early-init.el
+++ early-init.el
@@ -2,7 +2,7 @@
 ;; The default is 800 kilobytes.  Measured in bytes.
 (setq gc-cons-threshold (* 50 1000 1000))
 
-
+;; Theme
 (load-theme 'modus-vivendi t)
 
 ;; Packages
@@ -13,9 +13,10 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)	   
-;; (setq initial-frame-alist
-      ;; '((top . 1) (right . 1) (width . 80) (height . 60)))
-;; (set-face-attribute 'default nil :height 130)
+(setq initial-frame-alist
+      '((top . 1) (right . 1) (width . 106) (height . 72)))
+(set-face-attribute 'default t :font "Fira Code" :height 130)
 
 ;; Always start maximized
-(add-to-list 'default-frame-alist '(fullscreen . maximized))
+;; (add-to-list 'default-frame-alist '(fullscreen . maximized))
+
