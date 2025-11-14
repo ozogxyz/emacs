4c388bafe276cbde69a82b95ba8b9cb09823959f
ozogxyz <50372354+ozogxyz@users.noreply.github.com>
2025-11-14 22:21:57 +0300

    Consistent use-package, general cleanup, fix Windows packages and git

--- early-init.el
+++ early-init.el
@@ -1,4 +1,2 @@
 (setq gc-cons-threshold most-positive-fixnum)
-(setq package-enable-at-startup nil)
 (setq read-process-output-max (* 1024 1024))
-      
--- init.el
+++ init.el
@@ -1,48 +1,68 @@
-;; Custom file
+;; Load custom file
 (setq custom-file (concat user-emacs-directory "custom.el"))
 (load custom-file 'noerror 'nomessage)
 
-;; Load theme only in graphics mode
-;; (add-to-list 'custom-theme-load-path
-;;   (concat user-emacs-directory "themes"))
-;; ;; (when (display-graphic-p)
-;;   (load-theme 'gruber-darker))
-;; (set-face-foreground 'default "#FFFFFF")
+;; ----- Package setup -----
+(require 'package)
+(setq package-archives
+      '(("melpa" . "https://melpa.org/packages/")
+        ("gnu" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
 
-;; Bell
-(setq ring-bell-function 'ignore)
+;; ----- Windows Git fix -----
+(setenv "HOME" (getenv "USERPROFILE"))
+(setenv "PATH" (concat "D:/MINGIT/cmd;" (getenv "PATH")))
+(setq exec-path (cons "D:/MINGIT/cmd" exec-path))
 
-;; Clipboard
-(setq select-enable-clipboard t)
-(setq select-enable-primary t)
+;; ----- Theme -----
+(add-to-list 'custom-theme-load-path
+             (concat user-emacs-directory "themes"))
+(when (display-graphic-p)
+  (load-theme 'gruber-darker t))
+(set-face-foreground 'default "#FFFFFF")
 
-;; Some defaults
-(fset 'yes-or-no-p 'y-or-n-p)
-(ido-mode t)
-(setq default-directory "D:/PROG")
-(setq create-lockfiles nil
+;; ----- Defaults -----
+(setq default-directory "D:/PROG"
+      create-lockfiles nil
       inhibit-startup-message t
       inhibit-startup-screen t
       make-backup-files nil
-      auto-save-default nil)
+      auto-save-default nil
+      show-paren-delay 0
+      ring-bell-function 'ignore
+      select-enable-clipboard t
+      select-enable-primary t)
+(fset 'yes-or-no-p 'y-or-n-p)
+(ido-mode t)
+(show-paren-mode 1)
 
-;; Only load Magit manually
-(autoload 'magit-status "magit" nil t)
-
-;; Only load which-key manually
-(autoload 'which-key-mode "which-key" nil t)
-
-;; Load multiple-cursors automatically
+;; ----- Packages -----
 (use-package multiple-cursors
-  :ensure t
-  :bind (("C->" . mc/mark-next-like-this)
-         ("C-<" . mc/mark-previous-like-this)
-         ("C-c C-<" . mc/mark-all-like-this)))
+  :bind (("C-S-c C-S-c" . mc/edit-lines)
+         ("C->"         . mc/mark-next-like-this)
+         ("C-<"         . mc/mark-previous-like-this)
+         ("C-c C-<"     . mc/mark-all-like-this)
+         ("C-\""        . mc/skip-to-next-like-this)
+         ("C-:"         . mc/skip-to-previous-like-this)))
 
-;; Some key bindigs
-(global-set-key (kbd "M-[") (quote next-buffer))
-(global-set-key (kbd "M-]") (quote previous-buffer))
-(global-set-key (kbd "C-,") (quote duplicate-line))
-(put 'upcase-region 'disabled nil)
+(use-package company
+  :ensure t
+  :init
+  (global-company-mode 1)
+  :config
+  (setq company-minimum-prefix-length 1
+        company-tooltip-align-annotations t))
+
+;; ----- Keybindings -----
+(global-set-key (kbd "C-x C-;") 'comment-or-uncomment-region)
+(global-set-key (kbd "M-[") 'next-buffer)
+(global-set-key (kbd "M-]") 'previous-buffer)
+(global-set-key (kbd "C-'") 'duplicate-line)
 (global-set-key (kbd "C-c w") 'whitespace-mode)
 (global-set-key (kbd "C-x g") 'magit-status)
+(put 'upcase-region 'disabled nil)
